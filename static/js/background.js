d3d68bbe95daa7d72829d09f84930925fbe4f5fa
조성현 <scvzxc12@gmail.com>
2022-02-21 07:56:59 +0900

    Update background.js

--- static/js/background.js
+++ static/js/background.js
@@ -4,6 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `../JOsung-hyun.github.io/MOMENTUM/img/${chosenImage}`;
+bgImage.src = `../JOsung-hyun.github.io/static/img/${chosenImage}`;
 
 document.body.appendChild(bgImage)
